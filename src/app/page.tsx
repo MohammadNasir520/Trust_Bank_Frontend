bc47afb6ab2d028575ee69f9aae817dc4368b1b6
SanyFaysal <tfoysalahmed@gmail.com>
2024-01-20 20:39:07 +0600

    [Added] :Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import PageLayout from "@/components/layout/PageLayout";
 import Image from "next/image";
 
 export default function Home() {
   return (
-
-  <>Hello World</>
+    <PageLayout>
+      <div>Hellow</div>
+    </PageLayout>
   )
 }
