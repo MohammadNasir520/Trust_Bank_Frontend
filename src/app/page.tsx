dd80c1a9f4a3b1db93121a691a2a2a5563e966f4
AH-Likhon <ictianlikhon6@gmail.com>
2024-01-20 23:11:45 +0600

    [DONE] hero section slider using react-slick

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-import Image from "next/image";
+"use client";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import SliderCarousel from "../components/UI/slider/Slider";
 
 export default function Home() {
   return (
-
-  <>Hello World</>
-  )
+    <>
+      <SliderCarousel></SliderCarousel>
+    </>
+  );
 }
