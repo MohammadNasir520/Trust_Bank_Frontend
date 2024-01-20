9b4bd8f835c48cdb0648b1a7cca919a840c704a0
unknown <syednew5000@gmail.com>
2024-01-20 14:56:19 +0600

    folder structure

--- helpers/config/envConfig.ts
+++ helpers/config/envConfig.ts
@@ -1,4 +1,12 @@
-export const getBaseUrl = ():string => {
-    
-    return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api/v1"
-}
+export const getBaseUrl = (): any => {
+    const environment = process.env.NODE_ENV;
+
+    switch (environment) {
+        case 'development':
+            return 'http://localhost:6060/api/v1'; // Replace with your development API URL
+        case 'production':
+            return process.env.NEXT_PUBLIC_API_BASE_URL;// Replace with your production API URL
+        default:
+            return ' http://localhost:6060/api/v1'; // Replace with a default API URL
+    }
+};
