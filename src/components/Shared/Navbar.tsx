e14dbb4874cbbbd9d1ebc64226762dc93e03aac3
SanyFaysal <tfoysalahmed@gmail.com>
2024-01-21 21:57:27 +0600

    [DONE] : Navbar responsive

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -1,40 +1,48 @@
 import Image from "next/image";
 import Link from "next/link";
-import { BiNotification } from "react-icons/bi";
+import { BiMenu, BiNotification } from "react-icons/bi";
 import { FaSearch, FaWhatsapp } from "react-icons/fa";
 import { IoMdNotificationsOutline } from "react-icons/io";
 
 export default function Navbar() {
+
+    const infoItems = <div className="flex  justify-end gap-4 lg:mt-0 mt-3 lg:pr-0 pr-2 text-xl lg:text-white text-black">
+        <button>
+            <FaWhatsapp />
+        </button>
+        <button>
+            <FaSearch />
+        </button>
+        <button>
+            <IoMdNotificationsOutline />
+        </button>
+    </div>
     return (
         <div className="sticky top-0 z-[99] h-[105px] 	bg-white  ">
             <div className=" ">
                 <div className="grid grid-cols-8 overflow-hidden ">
-                    <div className="bg-[#1F305E] py-3 col-span-2  pl-20  rounded-br-2xl ml-[-30px]" style={{ transform: 'skew(-30deg)' }}>
-                        <Image src={'/logo-no-background.png'} height={170} width={200} alt="logo" className=" " style={{ transform: 'skew(30deg)' }} />
+                    <div className="
+                    bg-[#1F305E] py-3 lg:col-span-2 col-span-4
+                    lg:pl-20 pl-10 flex items-center gap-3 rounded-br-2xl ml-[-30px]
+                    p-16" style={{ transform: 'skew(-30deg)' }}>
+                        <p style={{ transform: 'skew(30deg)' }} className="text-2xl lg:hidden text-white"><BiMenu /></p>
+                        <Image src={'/logo-no-background.png'} height={170} width={200}
+                            alt="logo" className="" style={{ transform: 'skew(30deg)' }} />
                     </div>
-                    <div className=" col-span-6 h-full  ">
+                    <div className=" lg:col-span-6 col-span-4 h-full  ">
                         <div className="flex flex-col justify-between w-full      ">
-                            <div className="bg-[#1F305E] text-white py-2 flex justify-between h-1/2  w-full pr-16">
-                                <div className="flex gap-5 ml-6">
+                            <div className="bg-[#1F305E] text-white py-2  flex justify-between h-1/2  w-full pr-16">
+                                <div className="lg:flex hidden gap-5 ml-6">
                                     <p>Dashboard</p>
                                     <span>About</span>
 
                                 </div>
-
-                                <div className="flex justify-end gap-4 text-xl">
-                                    <button>
-                                        <FaWhatsapp />
-                                    </button>
-                                    <button>
-                                        <FaSearch />
-                                    </button>
-                                    <button>
-                                        <IoMdNotificationsOutline />
-                                    </button>
+                                <div className="lg:block hidden">
+                                    {infoItems}
 
                                 </div>
                             </div>
-                            <div className="flex justify-between my-auto p-5 text-black h-1/2  w-full  ">
+                            <div className="lg:flex hidden justify-between my-auto p-5 text-black h-1/2  w-full  ">
                                 <div className="flex   gap-5">
                                     <span>Loan</span>
                                     <span>Investment</span>
@@ -48,6 +56,7 @@ export default function Navbar() {
                                     <p>Open An Account</p>
                                 </div>
                             </div>
+                            <div className="lg:hidden   flex justify-end"> {infoItems}</div>
                         </div>
 
                     </div>
