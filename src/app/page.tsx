6e06120affc01741209959b987b79cadf98249b6
MohammadNasir520 <nasirahsan520@gmail.com>
2024-01-21 19:25:45 +0600

    tab create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import DebitCreditCard from "@/components/UI/Debit-Credit-card/DebitCreditCard";
+import DebitCreditCardTab from "@/components/UI/Debit-Credit-card/DebitCreditCardTab";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen grid lg:grid-cols-3 ">
-      <DebitCreditCard></DebitCreditCard>
-      <DebitCreditCard></DebitCreditCard>
-      <DebitCreditCard></DebitCreditCard>
-      <DebitCreditCard></DebitCreditCard>
-      <DebitCreditCard></DebitCreditCard>
-      <DebitCreditCard></DebitCreditCard>
+    // <div className="flex justify-center items-center h-screen grid lg:grid-cols-3 ">
+    //   <DebitCreditCard></DebitCreditCard>
+    //   <DebitCreditCard></DebitCreditCard>
+    //   <DebitCreditCard></DebitCreditCard>
+    //   <DebitCreditCard></DebitCreditCard>
+    //   <DebitCreditCard></DebitCreditCard>
+    //   <DebitCreditCard></DebitCreditCard>
+    // </div>
+    <div>
+      <DebitCreditCardTab></DebitCreditCardTab>
     </div>
   );
 }
