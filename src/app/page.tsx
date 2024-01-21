b0e3e3c133a9654eae92ffc8f7347677def59ad6
MohammadNasir520 <nasirahsan520@gmail.com>
2024-01-21 20:41:43 +0600

    correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import DebitCreditCard from "@/components/UI/Debit-Credit-card/DebitCreditCard";
-import DebitCreditCardTab from "@/components/UI/Debit-Credit-card/DebitCreditCardTab";
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    // <div className="flex justify-center items-center h-screen grid lg:grid-cols-3 ">
-    //   <DebitCreditCard></DebitCreditCard>
-    //   <DebitCreditCard></DebitCreditCard>
-    //   <DebitCreditCard></DebitCreditCard>
-    //   <DebitCreditCard></DebitCreditCard>
-    //   <DebitCreditCard></DebitCreditCard>
-    //   <DebitCreditCard></DebitCreditCard>
-    // </div>
-    <div>
-      <DebitCreditCardTab></DebitCreditCardTab>
-    </div>
-  );
+  return <>hello</>;
 }
--- src/components/UI/Debit-Credit-card/DebitCreditCardTab.tsx
+++ src/components/UI/Debit-Credit-card/DebitCreditCardTab.tsx
@@ -91,16 +91,87 @@ const DebitCreditCardTab = () => {
         },
       ],
     },
+    {
+      name: "popular",
+      panelData: [
+        {
+          title: "Savings Account",
+          description: "ZERO charges on ATM transactions",
+          image: loanImg,
+        },
+        {
+          title: "Savings Account",
+          description: "ZERO charges on ATM transactions",
+          image: loanImg,
+        },
+        {
+          title: "Savings Account",
+          description: "ZERO charges on ATM transactions",
+          image: loanImg,
+        },
+        {
+          title: "Savings Account",
+          description: "ZERO charges on ATM transactions",
+          image: loanImg,
+        },
+      ],
+    },
+    {
+      name: "popular",
+      panelData: [
+        {
+          title: "Savings Account",
+          description: "ZERO charges on ATM transactions",
+          image: loanImg,
+        },
+        {
+          title: "Savings Account",
+          description: "ZERO charges on ATM transactions",
+          image: loanImg,
+        },
+        {
+          title: "Savings Account",
+          description: "ZERO charges on ATM transactions",
+          image: loanImg,
+        },
+        {
+          title: "Savings Account",
+          description: "ZERO charges on ATM transactions",
+          image: loanImg,
+        },
+      ],
+    },
+    {
+      name: "popular",
+      panelData: [
+        {
+          title: "Savings Account",
+          description: "ZERO charges on ATM transactions",
+          image: loanImg,
+        },
+
+        {
+          title: "Savings Account",
+          description: "ZERO charges on ATM transactions",
+          image: loanImg,
+        },
+        {
+          title: "Savings Account",
+          description: "ZERO charges on ATM transactions",
+          image: loanImg,
+        },
+      ],
+    },
   ];
 
   return (
-    <div className="bg-blue-700 min-h-screen ">
+    <div className=" min-h-screen ">
       <Tabs size="md" variant="enclosed" className="px-[150px] py-3">
-        <TabList className=" backdrop-blur-md bg-white/30 text-white rounded-t-xl ">
+        <TabList className=" backdrop-blur-md bg-white/85 text-white rounded-t-xl ">
           {tabData?.map((tab) => (
             <Tab
-              className="flex items-center justify-center h-20 w-20 relative text-blue-950  hover:bg-white/50 "
-              _selected={{ color: "white", bg: "blue" }}
+              className="flex items-center rounded  justify-center h-16 w-32 relative text-blue-950  hover:bg-white/50 "
+              _selected={{ color: "white", bg: "blue", font: "bold" }}
             >
               <h1> {tab?.name}</h1>
               <div
