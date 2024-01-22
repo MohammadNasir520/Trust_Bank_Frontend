88d7518a0742b35d17e0b5e301184b92b003490b
AH-Likhon <ictianlikhon6@gmail.com>
2024-01-22 11:54:35 +0600

    [DONE] signin and signup with reusable input and button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import SliderCarousel from "../components/UI/slider/Slider";
+import AuthUI from "@/components/UI/AuthUI/page";
 
 export default function Home() {
   return (
     <>
-      <SliderCarousel></SliderCarousel>
+      {/* <SliderCarousel></SliderCarousel> */}
+      <AuthUI />
     </>
   );
 }
