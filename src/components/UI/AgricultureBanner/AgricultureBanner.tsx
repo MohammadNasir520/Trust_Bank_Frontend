921a3fbc33ff123276ddb312d760df332e2282e1
MohammadNasir520 <nasirahsan520@gmail.com>
2024-01-22 15:50:10 +0600

    agriculter banner

---
+++ src/components/UI/AgricultureBanner/AgricultureBanner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AgricultureBanner = () => {
+  return (
+    <div>
+      <h1>Agiriculture banner</h1>
+    </div>
+  );
+};
+
+export default AgricultureBanner;
