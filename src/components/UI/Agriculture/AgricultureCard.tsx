907a0e23d0b379915dc32355eec0abd4d90d651d
MohammadNasir520 <nasirahsan520@gmail.com>
2024-01-23 16:51:17 +0600

    banner image floting text added

--- src/components/UI/Agriculture/AgricultureCard.tsx
+++ src/components/UI/Agriculture/AgricultureCard.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import React from "react";
 import agriImg1 from "@/assets/images/agriImg1.jpg";
-import Button from "@/components/Common/Button";
 
 const AgricultureCard = () => {
   return (
     <div>
-      <div className="flex w-[600px] h-[300px] shadow-lg border">
+      <div className="flex w-[600px] h-[300px] shadow-lg border hover:shadow-xl ">
         <div className="h-full w-1/2 overflow-hidden ">
           <Image
             src={agriImg1}
@@ -14,18 +13,24 @@ const AgricultureCard = () => {
             height={500}
             alt="agri image"
             // layout="responsive"
-            className="h-full overflow-hidden  hover:scale-110 transition-transform transform ease-out"
+            className="h-full overflow-hidden cursor-pointer hover:scale-110 transition-transform transform ease-out"
           ></Image>
         </div>
-        <div className="w-1/2">
-          <h1>Title</h1>
-          <p>
+        <div className="w-1/2 flex flex-col space-y-5 p-4 relative ">
+          <img
+            src="https://docs.material-tailwind.com/img/face-2.jpg"
+            alt="avatar"
+            className="relative inline-block h-12 w-12 !rounded-full  object-cover object-center"
+          />
+          <h1>Agricultural Jewel Loan Scheme</h1>
+          <p className="text-gray-500">
             Purpose / Objective: To augment flow of credit to tenant farmers
             cultivating land either as oral lessees or share croppers…
           </p>
 
-          {/* <Button>Read More</Button> */}
-          <button className="h-11 w-12">Read More</button>
+          <button className="bg-[#1F305E] px-4 py-2   text-white absolute bottom-0 w-2/3">
+            Read More
+          </button>
         </div>
       </div>
     </div>
--- src/components/pages/Agriculture/Agriculture.tsx
+++ src/components/pages/Agriculture/Agriculture.tsx
@@ -4,9 +4,15 @@ import React from "react";
 
 const Agriculture = () => {
   return (
-    <div className="min-h-screen">
+    <div className="">
       <AgricultureBanner></AgricultureBanner>
-      <div className="px-11">
+
+      <div className="px-11 grid md:grid-cols-2 gap-10 my-3">
+        <AgricultureCard></AgricultureCard>
+        <AgricultureCard></AgricultureCard>
+        <AgricultureCard></AgricultureCard>
+        <AgricultureCard></AgricultureCard>
+        <AgricultureCard></AgricultureCard>
         <AgricultureCard></AgricultureCard>
       </div>
     </div>
