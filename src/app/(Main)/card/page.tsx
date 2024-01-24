af17c2750b7699c8d7b38368e9d29c7c875bc525
unknown <syednew5000@gmail.com>
2024-01-24 19:44:07 +0600

    done update ui

---
+++ src/app/(Main)/card/page.tsx
@@ -0,0 +1,31 @@
+import PageLayout from "@/components/layout/PageLayout";
+import Card from "@/components/pages/card_page/Card";
+import CardBanner from "@/components/ui/slider/CardBanner";
+
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
+const page = () => {
+  return (
+    <>
+     
+        <CardBanner />
+        <div className="text-center mt-8">
+          <h1 className="text-center text-3xl font-semibold">
+            Pick Your Best debit & credit card
+          </h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-col-2 lg:grid-cols-3 w-[80%] mx-auto my-4">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+     
+    </>
+  );
+};
+
+export default page;
