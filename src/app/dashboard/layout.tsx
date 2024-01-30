6f2dd6650d2057e84aa6e589cda31acfd30611cb
SanyFaysal <tfoysalahmed@gmail.com>
2024-01-30 20:31:23 +0600

    [DONE] - Dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+'use client'
+import Header from "@/components/dashboard/partials/Header";
+import Sidebar from "@/components/dashboard/partials/Sidebar";
+import { useState } from "react";
+
+
+function Dashboard({ children }: { children: React.ReactElement | React.ReactNode }) {
+
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    return (
+        <div className="flex h-screen overflow-hidden">
+            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+                <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+                <main>
+                    <div className="px-4   sm:px-6  rounded  lg:px-8 py-8 w-full max-w-9xl ">
+                        {children}
+                    </div>
+                </main>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ src/components/dashboard/partials/DashboardMenu.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import Link from "next/link";
+import SidebarLinkGroup from "./SidebarLinkGroup";
+import { usePathname, useRouter } from "next/navigation";
+
+interface IDashboardMenu {
+    pathname: string;
+    icon?: React.ReactElement;
+    title: string;
+    sidebarExpanded: boolean;
+    setSidebarExpanded: any;
+    children?: React.ReactElement[] | React.ReactNode
+
+}
+export default function DashboardMenu({
+    pathname, title, icon, sidebarExpanded, setSidebarExpanded, children
+}: IDashboardMenu) {
+    const router = useRouter()
+    const currentPath = usePathname();
+
+
+    const handleMenuOpen = (
+        e: any, handleClick: any,
+        children: any, pathname: string
+    ) => {
+        e.preventDefault();
+        sidebarExpanded ? handleClick() : setSidebarExpanded(true);
+        if (!children) {
+            router.push(pathname)
+        }
+    }
+
+    return (
+        <SidebarLinkGroup activecondition={currentPath.includes(pathname)}>
+            {(handleClick: any, open: any) => {
+                return (
+                    <>
+                        <div
+                            className={`block cursor-pointer text-slate-200 truncate transition duration-150 ${pathname.includes('ecommerce') ? 'hover:text-slate-200' : 'hover:text-white'
+                                }`}
+                            onClick={(e) => handleMenuOpen(e, handleClick, children, pathname)}
+                        >
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-center">
+                                    {icon}
+                                    <span className="text-sm font-medium ml-3  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                        {title}
+                                    </span>
+                                </div>
+                                {/* Icon */}
+                                {children && <div className="flex shrink-0 ml-2">
+                                    <svg className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${open && 'rotate-180'}`} viewBox="0 0 12 12">
+                                        <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
+                                    </svg>
+                                </div>}
+                            </div>
+                        </div>
+                        {children &&
+                            <div className={` ${sidebarExpanded ? 'lg:block' : 'lg:hidden'} 2xl:block `}>
+                                <ul className={`pl-9 mt-1 ${!open && 'hidden'} text-slate-100`}>
+                                    {children}
+                                </ul>
+                            </div>}
+                    </>
+                );
+            }}
+        </SidebarLinkGroup>
+    )
+}
