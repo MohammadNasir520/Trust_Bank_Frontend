ee6dfec34610f0752b40133c861a0825d5125935
AH-Likhon <ictianlikhon6@gmail.com>
2024-02-02 01:41:05 +0600

    [DONE] currency calculator is working now as dynamically

--- src/components/pages/CurrencyCalculator/Calculator/CurrencyField.tsx
+++ src/components/pages/CurrencyCalculator/Calculator/CurrencyField.tsx
@@ -1,25 +1,28 @@
-"use client";
-
+// components/CurrencyField.tsx
 import { Text } from "@chakra-ui/react";
 import Image from "next/image";
-import { useState } from "react";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
-const currenciesData = [
-  { flag: "/assets/united-kingdom-icon.webp", currency: "USD" },
-  { flag: "/assets/united-kingdom-icon.webp", currency: "GBP" },
-];
+interface CurrencyFieldProps {
+  currency: string;
+  onChange?: (amount: number) => void;
+  placeholder: string;
+  value?: number;
+  readOnly?: boolean;
+}
 
-const CurrencyField = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleOptionClick = (option: any) => {
-    console.log(`Selected option: ${option}`);
-    setIsOpen(false);
+const CurrencyField: React.FC<CurrencyFieldProps> = ({
+  currency,
+  onChange,
+  placeholder,
+  value,
+  readOnly,
+}) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const amount = Number(e.target.value);
+    if (!isNaN(amount) && onChange) {
+      onChange(amount);
+    }
   };
 
   return (
@@ -30,51 +33,20 @@ const CurrencyField = () => {
           alt=""
           width={100}
           height={100}
-          src="/assets/usa-icon.webp"
+          src={`/assets/${currency.toLowerCase()}-icon.webp`}
         />
-
         <input
           type="text"
           className="w-full border border-gray-300 py-2 pr-2 pl-12 rounded-md text-gray-900 sm:text-sm sm:leading-6 placeholder:text-gray-900 focus:outline-none focus:rounded-bl-none focus:rounded-br-none bg-gray-50 appearance-none"
-          placeholder="USD"
-          onClick={toggleDropdown}
+          placeholder={placeholder}
+          onChange={handleInputChange}
+          value={value !== undefined ? value : ""}
+          readOnly={readOnly}
         />
         <div className="absolute right-4">
-          {isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
+          {readOnly ? null : <IoIosArrowDown />}
         </div>
       </div>
-      <div>
-        <input
-          type="text"
-          className="w-full border border-gray-300 py-2 px-4 rounded-md text-gray-900 sm:text-sm sm:leading-6 placeholder:text-gray-900 focus:outline-none bg-gray-50 appearance-none"
-          placeholder="Amount to transfer"
-        />
-      </div>
-
-      {isOpen && (
-        <div className="origin-top-right absolute right-0 top-[41px] w-full rounded-tl-none rounded-tr-none rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50">
-          <div className="py-0" role="none">
-            {/* Dropdown items */}
-            {currenciesData.map((el, index) => (
-              <div
-                key={index}
-                className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 cursor-pointer flex items-center gap-1"
-                role="menuitem"
-                onClick={() => handleOptionClick(el.currency)}
-              >
-                <Image
-                  className="w-6 h-6"
-                  alt=""
-                  width={100}
-                  height={100}
-                  src={el.flag}
-                />
-                <Text>{el.currency}</Text>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
     </div>
   );
 };
